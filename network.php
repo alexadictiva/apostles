cb57e73d7678ee5e347c5c6b2eb5897a78089b1d
Libia Perez <alexandra220790@gmail.com>
2021-09-30 10:51:50 -0300

    Creating Network html structure

--- network.php
+++ network.php
@@ -4,12 +4,44 @@ include './components/header.php';
 
 ?>
 
-
-
-
-<h1>
-  Network
-</h1>
+<main>
+  <section id="network">
+    <h1>NETWORK</h1>
+    <div class="map-container">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.910440974647!2d-79.52242598627652!3d8.980388493554573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca8e3edc9e20d%3A0x7f7d590dc09caff!2sWorld%20Trade%20Center%20Panama!5e0!3m2!1ses-419!2sar!4v1633007095502!5m2!1ses-419!2sar"
+        width="100%"
+        height="450"
+        allowfullscreen=""
+        loading="lazy"
+      ></iframe>
+    </div>
+    <div class="description-network">
+      <p>
+        The Twelve Apostles Inc. has organized the logistic of the company, in
+        different strategic locations in order to achieve regional integration
+        and create an ideal resource for this kind of business.
+        <br />
+        Our head office is located in Panama City (Panama) and the distribution
+        centers branches are located in different cities achieving vertical
+        integration: Asunción (Paraguay), Buenos Aires (Argentina) and
+        Montevideo (Uruguay).
+      </p>
+    </div>
+    <div class="description-cloud-container">
+      <div class="description-cloud-content">
+        <p>
+          ¨WE ARE A REGIONAL DISTRIBUTOR OF INTERNATIONAL BRANDS SPECIALIZING IN
+          MULTIPLE MARKET CHANNELS, RELATED TO MARKET LEADERS AND ALWAYS
+          ACHIEVING TO PROVIDE THE BEST PRODUCT / PRICE COMBINATION¨
+        </p>
+      </div>
+      <div class="description-cloud-author">
+        <p>The Twelve Apostles Inc. Director</p>
+      </div>
+    </div>
+  </section>
+</main>
 
 
 
