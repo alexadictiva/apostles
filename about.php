db36a80a6e23249cb01379bf5066c68d80104c9b
Libia Perez <alexandra220790@gmail.com>
2021-10-31 15:05:25 -0300

    fixing styles

--- about.php
+++ about.php
@@ -9,9 +9,9 @@ include './components/header.php';
     <hr />
     <div class="about-cards-container">
       <article class="card-container card-container1">
-        <div class="wow animate__animated animate__fadeInUp card card1">
+        <div class=" card card1">
           <h3>Our Vision</h3>
-          <p>
+          <p class="wow animate__animated animate__fadeInUp">
             We are a long experience company in distributing international
             brands. We specialized in regional sale of a wide variety of
             products. During the years we have been in the market, we have
@@ -25,9 +25,9 @@ include './components/header.php';
         </div>
       </article>
       <article class="card-container card-container2">
-        <div class="wow animate__animated animate__fadeInUp card card2">
+        <div class=" card card2">
           <h3>Our Team</h3>
-          <p>
+          <p class="wow animate__animated animate__fadeInUp">
             Our company is staffed by experienced and trained professionals. We
             have a team with background in distribution business, economic
             resources and marketing strategies. Our company is also organized in
@@ -41,9 +41,9 @@ include './components/header.php';
         </div>
       </article>
       <article class="card-container card-container3">
-        <div class="wow animate__animated animate__fadeInUp card card3">
+        <div class=" card card3">
           <h3>Our Mision</h3>
-          <p>
+          <p class="wow animate__animated animate__fadeInUp">
             We provide to our customers and retailers a personalized service
             offering them marketing, sales campaign and any kind of commercial
             support. Our mission is to develop long term business relationships
